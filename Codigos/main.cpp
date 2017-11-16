ed3107c1ed224e8c18793d7e41169b857828d572
Felipe Melchior <fehmel@gmail.com>
2017-11-16 16:32:41 -0200

    Main criada

---
+++ Codigos/main.cpp
@@ -0,0 +1,24 @@
+/*
+	AUTORES
+		FELIPE HOMRICH MELCHIOR
+		LUCAS ANTUNES DE ALMEIDA
+		LUCAS MATEUS DOS SANTOS MELO
+		RAFAEL FERNANDES DA SILVA
+
+
+	UNIPAMPA - CAMPUS ALEGRETE
+*/
+
+#include <iostream>
+
+#include "menu.hpp"
+
+using namespace std;
+
+int main(int argc, char **argv){
+	Menu *menu = new Menu();
+
+	menu->exibeMenu();
+
+	return 0;
+}
